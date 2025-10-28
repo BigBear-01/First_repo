83ac1bb793557857a52f034f028b944014e90f04
BB_ <evanfernando785@gmail.com>
2025-10-28 13:01:55 +0700

    First up

---
+++ helloworld.py
@@ -0,0 +1,72 @@
+# Strings
+
+name = "Nando"
+fav_food = "Breast chicken"
+hobby = "Lifting weights"
+game = "Spider Man Miles morales"
+food = "chocolate bar"
+
+# example for strings
+print(f"My name is {name}")
+print(f"my hobby should be {hobby}")
+print(f"and absolutely {fav_food} was my favorite food")
+print(f"I love playing {game} also")
+
+# f stand for format
+
+# Integers
+
+age = 18
+height = 176 
+weight = 82
+ftsz = 44
+quantity = 5
+
+# example for integers
+print(f"Now im {age} years old")
+print("i would tell u guys something abt my personal stats")
+print(f"i am {height} cm")
+print(f"and {weight} KG")
+print(f"i have abt {quantity} shoes")
+print(f"and my foot size was abt {ftsz}")
+
+# Float
+
+price = 19.99
+p_time = 18.5
+gpa = 4.00
+
+# ex for float
+print(f"i love to play {game} and it cost ${price} on steam")
+print(f"and i have play that game for {p_time} hours")
+print(f"while i eat my {food}")
+
+# BooLean
+
+gymguy = False
+love_girls = True
+pervert = False
+
+# ex
+if love_girls : 
+ print("i love girls")
+else :
+ print("he is not love girls")
+
+if gymguy :
+ print("im a gym guy")
+else:
+ print("im not a gym guy")
+
+if pervert :
+ print("i loved to see nude girls")
+else:
+ print("i always treating girls nicely")
+
+
+#Input() = A function that promts the user to enter data 
+#          Returns entered data as a string
+# EX
+input("What is your name?: ")
+name = input("What is your name?: ")
+print(f"Hello {name}!!")
